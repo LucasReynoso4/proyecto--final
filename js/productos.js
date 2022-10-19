42093cb29c63a2ac97fe4d1313f0c588a36242c5
lucasreynoso <lucassreynoso4@gmail.com>
2022-10-18 23:41:40 -0300

    productos

--- js/productos.js
+++ js/productos.js
@@ -1,8 +1,43 @@
 const productos = [{
-    id:1
-    foto: "../img/hamburguesaalbum.jpg",
-    nombre: 
+    id: 1,
+    foto: "http://127.0.0.1:5500/img/hambuguesaalbum.jpg",
+    nombre: "Cámara web Xiaomi",
+    precio: 1300,
+},
+{
+    id: 2,
+    foto: "http://127.0.0.1:5500/img/pizzaalbum.jpg",
+    nombre: "Pizza jamon y morron",
+    precio: 1000,
+
+
+},
+{
+    id: 3,
+    foto: "http://127.0.0.1:5500/img/empanadas.jpg",
+    nombre: "Docena de empanadas carne,jyq y pollo",
+    precio: 900,
+},
+{
+    id:4,
+    foto: "http://127.0.0.1:5500/img/pastasalbum.png",
+    nombre: "pastas",
+    precio: 1000,
+},
+{
+    id:5,
+    foto: "http://127.0.0.1:5500/img/tartasegunda.png",
+    nombre: "tartas",
+    precio: 950
+},
+{
+    id:6,
+    foto: "http://127.0.0.1:5500/img/postresalbum.png",
+    nombre: "postres",
+    precio: 600
 }
+
+
 
 
 
